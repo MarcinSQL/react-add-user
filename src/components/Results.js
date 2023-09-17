eb2b3e9d0a36684c07543504080d3edf04f6fe85
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-09-17 23:27:13 +0200

    Added some styles and semantic to Results component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,6 @@
 import Result from "./Result";
 import Card from "./UI/Card";
+import classes from "./Results.module.css";
 
 const Results = (props) => {
   return (
@@ -7,9 +8,13 @@ const Results = (props) => {
       {props.userInputs == 0 ? (
         <p>No users entered</p>
       ) : (
-        props.userInputs.map((data) => {
-          return (<Result key={data.id} username={data.username} age={data.age} />);
-        })
+        <ul className={classes.list}>
+          {props.userInputs.map((data) => {
+            return (
+              <Result key={data.id} username={data.username} age={data.age} />
+            );
+          })}
+        </ul>
       )}
     </Card>
   );

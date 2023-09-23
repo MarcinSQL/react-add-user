34cc4299397c284f997103fe4549b71222d0472e
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-09-23 19:38:51 +0200

    Moved (by createPortal) Modal components to body tag

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
+import { Fragment } from "react";
 
-const Modal = (props) => {
+const Backdrop = () => {
+  return <div className={classes.modal}></div>;
+};
+
+const ErrorModal = (props) => {
   const [exit, setExit] = useState(false);
 
   const hideModalHandler = () => {
@@ -12,14 +18,27 @@ const Modal = (props) => {
   };
 
   return (
-    <div className={classes.modal}>
-      <div className={classes.modal__content}>
-        <button className={classes.modal__exit} onClick={hideModalHandler}>
-          X
-        </button>
-        {props.children}
-      </div>
+    <div className={classes.modal__content}>
+      <button className={classes.modal__exit} onClick={hideModalHandler}>
+        X
+      </button>
+      {props.children}
     </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById("overlay-root")
+      )}
+      {ReactDOM.createPortal(
+        <ErrorModal exit={props.exit}>{props.children}</ErrorModal>,
+        document.getElementById("overlay-root")
+      )}
+    </Fragment>
   );
 };
 
